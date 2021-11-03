a067ab724d98a7fb0a5333b4df2aa49208f0aa3e
heyset <c.inaciomatheus@gmail.com>
2021-11-03 17:21:26 -0300

    Adiciona testes para adicionar task

---
+++ backend/__tests__/task/manage.spec.ts
@@ -0,0 +1,105 @@
+/* global jest, beforeEach, afterAll, afterEach */
+
+import { describe, expect, it } from '@jest/globals';
+import request from 'supertest';
+import { ObjectId } from 'mongodb';
+
+import app from '../../app';
+import connect, { disconnect } from '../../src/models/connect';
+import { closeCacheServer } from '../../src/middlewares/withCache';
+import { closeBlacklistServer } from '../../src/services/token';
+
+jest.mock('../../src/models/connect');
+
+afterAll(async () => {
+  await closeCacheServer();
+  await closeBlacklistServer();
+  await disconnect();
+});
+
+const url = '/task';
+
+type Task = {
+  title: string;
+  description: string;
+  createdAt: number;
+  lastModifiedAt: number;
+  status: 'to_do' | 'in_progress' | 'done';
+};
+
+const isEqualWithErrorMargin = (a : number, b : number, error : number) => {
+  const absoluteDifference = Math.abs(a - b);
+  return absoluteDifference < error;
+};
+
+describe('POST /task (create task)', () => {
+  const validUser = {
+    username: 'janete_corca',
+    email: 'janete@corca.com',
+    password: '123janete456corca',
+  };
+
+  const validTask = {
+    title: 'demolish buildings',
+    description: 'today is a good day to demolish some stuff! I love my job',
+  };
+
+  let accessToken : string;
+
+  beforeEach(async () => {
+    await request(app)
+      .post('/users')
+      .send(validUser)
+      .expect(201)
+      .then((response) => {
+        const cookies = (response.headers['set-cookie'] as Array<string>).reduce((acc : Record<string, string>, cookie : string) => {
+          const [type, fullDescription] = cookie.split('=');
+          const [value, ..._rest] = fullDescription.split(';');
+          acc[type] = value;
+          return acc;
+        }, {});
+
+        accessToken = cookies.access_token;
+      });
+  });
+
+  afterEach(async () => {
+    await connect()
+      .then((db) => db.collection('users').deleteMany({}));
+
+    await connect()
+      .then((db) => db.collection('tasks').deleteMany({}));
+  });
+
+  describe('With valid data, creates a task in the db', () => {
+    it('title and description', async () => {
+      let id : string;
+
+      await request(app)
+        .post(url)
+        .set('Authorization', accessToken)
+        .send(validTask)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.id).not.toBeUndefined();
+          id = res.body.id;
+        });
+
+      await connect()
+        .then((db) => db.collection('tasks'))
+        .then((collection) => collection.findOne(new ObjectId(id)) as Promise<Task>)
+        .then((foundTask : Task) => {
+          expect(foundTask.title).toBe(validTask.title);
+          expect(foundTask.description).toBe(validTask.description);
+          expect(isEqualWithErrorMargin(
+            foundTask.createdAt,
+            new Date().valueOf(),
+            600000,
+          )).toBe(true);
+
+          expect(foundTask.createdAt).toBe(foundTask.lastModifiedAt);
+          expect(foundTask.status).toBe('to_do');
+        });
+    });
+  });
+});
