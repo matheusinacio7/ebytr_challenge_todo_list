07e5341bb7f56058b649eb0584c790df3c1066b5
heyset <c.inaciomatheus@gmail.com>
2021-11-03 17:49:23 -0300

    Corrige schema

--- backend/src/services/validation/schemas/task.ts
+++ backend/src/services/validation/schemas/task.ts
@@ -2,7 +2,7 @@ const create = {
   type: 'object',
   properties: {
     title: { type: 'string', minLength: 1, maxLength: 100 },
-    description: { type: 'string', minLenght: 1, maxLength: 500 },
+    description: { type: 'string', minLength: 1, maxLength: 500 },
     createdAt: { type: 'number' },
     lastModifiedAt: { type: 'number' },
     status: { type: 'string', pattern: 'to_do' },
