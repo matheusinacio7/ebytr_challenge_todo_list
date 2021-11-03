3a44c29a4469d2072e4007e17a02c769cbb3935e
heyset <c.inaciomatheus@gmail.com>
2021-11-03 17:43:23 -0300

    Modifica model para usar tipagem de Task e adiciona controller para Task

---
+++ backend/src/controllers/Task.ts
@@ -0,0 +1,22 @@
+import { Task as Model } from '@models';
+import { validate } from '@validation';
+import type { Task } from '@types';
+
+const create = (coreTaskInfo: Pick<Task, 'title' | 'username' | 'description'>) => {
+  const now = new Date().valueOf();
+
+  const newTask : Task = {
+    ...coreTaskInfo,
+    createdAt: now,
+    lastModifiedAt: now,
+    status: 'to_do',
+  };
+
+  validate('createTask', newTask);
+
+  return Model.insertOne(newTask);
+};
+
+export default {
+  create,
+};
--- backend/src/models/Task.ts
+++ backend/src/models/Task.ts
@@ -1,10 +1,12 @@
 import type { Db } from 'mongodb';
 
+import type { Task } from '@types';
+
 import connect from './connect';
 
 const getCollection = (db : Db) => db.collection('tasks');
 
-const insertOne = (taskData: any) => connect()
+const insertOne = (taskData: Task) => connect()
   .then(getCollection)
   .then((collection) => collection.insertOne(taskData));
 
